46f3372b6342d7f4334bc3d6e11910ba89a36a9c
hvmq1333 <167736369+hvmq1333@users.noreply.github.com>
2025-07-05 07:45:16 +0700

    5/7

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3722072822"
+    serviceWorkerVersion: "2330144673"
   }
 });
